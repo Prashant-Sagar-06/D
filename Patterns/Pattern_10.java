4021ae5542d0189dea262b7e4c4bdae00b9cc50b
Prashant Sagar <prashant.sagar_cs.aiml23@gla.ac.in>
2024-10-31 08:48:44 +0530

    Add Pattern_10 for combined triangle and inverted triangle pattern generation

---
+++ Patterns/Pattern_10.java
@@ -0,0 +1,22 @@
+package Patterns;
+import java.util.Scanner;
+public class Pattern_10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n = sc.nextInt();
+        sc.close();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n-i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
