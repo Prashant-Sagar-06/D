03ee350058cfa56c2431a3197a64d12b9b27ac54
Prashant Sagar <prashant.sagar_cs.aiml23@gla.ac.in>
2024-11-01 16:10:03 +0530

    Add Pattern_19 for hourglass star pattern generation

---
+++ Patterns/Pattern_19.java
@@ -0,0 +1,34 @@
+package Patterns;
+import java.util.Scanner;
+public class Pattern_19 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int n = scn.nextInt();
+        scn.close();
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                System.out.print("*");
+            }
+            for (int j = 0; j < 2*i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 2*i; j < 2*n-2; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
