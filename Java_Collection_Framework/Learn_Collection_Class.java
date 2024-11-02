385228dc1f47f6101ac031cd107d7144c35e1e86
Prashant Sagar <prashant.sagar_cs.aiml23@gla.ac.in>
2024-11-02 20:39:09 +0530

    Enhance Learn_Collection_Class with additional collection operations

--- Java_Collection_Framework/Learn_Collection_Class.java
+++ Java_Collection_Framework/Learn_Collection_Class.java
@@ -1,19 +1,25 @@
 package Java_Collection_Framework;
 
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Learn_Collection_Class {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>(List.of(1, 2, 5, 8, 20));
+        List<Integer> list = new ArrayList<>(List.of(1, 2, 5, 5,5,58, 20));
         Collections.sort(list);
         System.out.println(list);
         System.out.println(Collections.binarySearch(list, 5));
         Collections.reverse(list);
         System.out.println(list);
+        System.out.println(Collections.max(list));
+        System.out.println(Collections.min(list));
+        System.out.println(Collections.frequency(list, 5));
+        Collections.sort(list, Comparator.reverseOrder());
+        System.out.println(list);
         Collections.fill(list, 0);
         System.out.println(list);
-        
+
     }
 }
