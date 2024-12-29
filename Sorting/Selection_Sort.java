ece0929774d0f15686a3f0e877b9c464fcf4b14b
Prashant Sagar <prashant.sagar_cs.aiml23@gla.ac.in>
2024-12-29 21:34:01 +0530

    Implement Selection Sort algorithm and enhance user input prompts for array elements

--- Sorting/Selection_Sort.java
+++ Sorting/Selection_Sort.java
@@ -1,13 +1,40 @@
 package Sorting;
-
 import java.util.Scanner;
 
 public class Selection_Sort {
+    public static void selectionSort(int[] arr){
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++) {
+            int min = i;
+            for (int j = i+1; j < n; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of elements in the array: ");
+        System.out.println("Enter the number of elements: ");
         int n = sc.nextInt();
         int[] arr = new int[n];
-        
+        System.out.println("Enter the elements: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Array before sorting: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        selectionSort(arr);
+        System.out.println("Array after sorting: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        sc.close();
     }
 }
