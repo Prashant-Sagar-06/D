f315253bd14470622ede0820ce7a6c414c564e5f
Prashant Sagar <prashant.sagar_cs.aiml23@gla.ac.in>
2025-01-02 20:29:24 +0530

    Add Bubble Sort algorithm implementation with user input for array elements

---
+++ Sorting/Bubble_Sort.java
@@ -0,0 +1,43 @@
+package Sorting;
+import java.util.Scanner;
+public class Bubble_Sort{
+    public static void BubbleSort(int [] arr){
+        int n = arr.length;
+        for (int i = n-1; i >= 0 ; i--) {
+            int didswap = 0;
+            for (int j = 0; j < i; j++) {
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    didswap = 1;
+                }
+            }
+            if (didswap == 0) {
+                break;
+                
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Array before sorting: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        BubbleSort(arr);
+        System.out.println("Array after sorting: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        sc.close();
+    }
+}
