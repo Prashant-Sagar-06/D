9bc998a6caa2f8f411fe6f0fb4c0d245609797e4
Prashant Sagar <prashant.sagar_cs.aiml23@gla.ac.in>
2025-01-02 20:38:43 +0530

    Add Insertion Sort algorithm implementation with user input for array elements

---
+++ Sorting/Insertion_Sort.java
@@ -0,0 +1,38 @@
+package Sorting;
+import java.util.Scanner;
+public class Insertion_Sort {
+    public static void InsertionSort(int [] arr){
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+
+            int j = i;
+            while(j>0 && arr[j-1] > arr[j]){
+                int temp = arr[j];
+                arr[j] = arr[j-1];
+                arr[j-1] = temp;
+                j--;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array: ");
+        int n = sc.nextInt();
+        int [] arr = new int[n];
+        System.out.println("Enter the elements of the array: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Array before sorting: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        InsertionSort(arr);
+        System.out.println("Array after sorting: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        sc.close(); 
+    }
+}
